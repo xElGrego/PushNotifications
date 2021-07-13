d4257a404006fce08dd6b2217436b46369ba7a33
xElGrego <elgrego977@gmail.com>
2021-07-12 23:54:50 -0500

    Preparando el proyecto

---
+++ src/app/components/list-productos/list-productos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListProductosComponent } from './list-productos.component';
+
+describe('ListProductosComponent', () => {
+  let component: ListProductosComponent;
+  let fixture: ComponentFixture<ListProductosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListProductosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListProductosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/list-productos/list-productos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-productos',
+  templateUrl: './list-productos.component.html',
+  styleUrls: ['./list-productos.component.css']
+})
+export class ListProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
